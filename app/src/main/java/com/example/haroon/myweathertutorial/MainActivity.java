d22aec6134a76895fc4a4ed8ff7018b7e4fd618b
yfoughal <yfoughali@coyote-group.com>
2018-11-28 11:25:21 +0100

    1ere version appli (Paris Alger Smeo)

--- app/src/main/java/com/example/haroon/myweathertutorial/MainActivity.java
+++ app/src/main/java/com/example/haroon/myweathertutorial/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.haroon.myweathertutorial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -17,10 +21,15 @@ import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView t1_temp,t2_city,t3_description,t4_date;
+    Button choix = null;
+    Button smeo = null;
+    ImageView icone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,13 +39,22 @@ public class MainActivity extends AppCompatActivity {
         t2_city = (TextView)findViewById(R.id.textView3);
         t3_description = (TextView)findViewById(R.id.textView4);
         t4_date = (TextView)findViewById(R.id.textView2);
+        choix = (Button)findViewById(R.id.choix);
+        icone = (ImageView)findViewById(R.id.imageView);
+        smeo = (Button)findViewById(R.id.smeo);
 
+
+
+        choix.setOnClickListener(choixListener);
+        smeo.setOnClickListener(smeoListener);
+
+        setTitle("Meteo");
         find_weather();
     }
 
     public void find_weather()
     {
-        String url ="http://api.openweathermap.org/data/2.5/weather?q=islamabad,pakistan&appid=90ebdc57172a838d0fce0abbc044df8e&units=Imperial";
+        String url ="http://api.openweathermap.org/data/2.5/weather?q=paris,france&appid=b7d1a5da596d0f045ad99a42e9d40d57&units=Imperial";
 
         JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -49,13 +67,54 @@ public class MainActivity extends AppCompatActivity {
                     String temp = String.valueOf(main_object.getDouble("temp"));
                     String description = object.getString("description");
                     String city = response.getString("name");
+                    //String id = String.valueOf(object.getInt("id"));
+                    String id = response.getString("id");
 
-                  //  t1_temp.setText(temp);
-                    t2_city.setText(city);
+                    t1_temp.setText(temp);
+                    t2_city.setText(city + "  " + id);
                     t3_description.setText(description);
 
+                    if(description.equals("scattered clouds"))
+                    {
+                        icone.setImageResource(R.drawable.icon_scatteredclouds);
+                    }
+                    if(description.equals("clear sky"))
+                    {
+                        icone.setImageResource(R.drawable.icon_clearsky);
+                    }
+
+                    if(description.equals("broken clouds"))
+                    {
+                        icone.setImageResource(R.drawable.icon_brokenclouds);
+                    }
+
+                    if(description.equals("overcast clouds"))
+                    {
+                        icone.setImageResource(R.drawable.icon_brokenclouds);
+                    }
+
+                    if(description.equals("rain"))
+                    {
+                        icone.setImageResource(R.drawable.icon_rain);
+                    }
+
+                    if(description.equals("snow"))
+                    {
+                        icone.setImageResource(R.drawable.icon_snow);
+                    }
+
+                    if(description.equals("mist"))
+                    {
+                        icone.setImageResource(R.drawable.icon_mist);
+                    }
+
+                    if(description.equals("light rain"))
+                    {
+                        icone.setImageResource(R.drawable.icon_rain);
+                    }
+
                     Calendar calendar = Calendar.getInstance();
-                    SimpleDateFormat sdf = new SimpleDateFormat("EEEE=MM-dd");
+                    SimpleDateFormat sdf = new SimpleDateFormat("EEEE dd/MM", Locale.FRANCE);
                     String formatted_date = sdf.format(calendar.getTime());
 
                     t4_date.setText(formatted_date);
@@ -86,6 +145,24 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private View.OnClickListener choixListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent ChoixActivity = new Intent(MainActivity.this, ChoixActivity.class);
+            startActivity(ChoixActivity);
+
+        }
+    };
+
+    private View.OnClickListener smeoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent SmeoActivity = new Intent(MainActivity.this, SmeoActivity.class);
+            startActivity(SmeoActivity);
+
+        }
+    };
 
 
 
---
+++ app/src/main/java/com/example/haroon/myweathertutorial/SmeoActivity.java
@@ -0,0 +1,124 @@
+package com.example.haroon.myweathertutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class SmeoActivity extends AppCompatActivity {
+
+    TextView t1_temp,t2_city,t3_description,t4_date;
+    ImageView icone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smeo);
+
+        t1_temp = (TextView) findViewById(R.id.textView);
+        t2_city = (TextView) findViewById(R.id.textView3);
+        t3_description = (TextView) findViewById(R.id.textView4);
+        t4_date = (TextView) findViewById(R.id.textView2);
+
+        icone = (ImageView) findViewById(R.id.imageView);
+
+
+        setTitle("Meteo SMEO");
+        find_weather();
+    }
+
+        public void find_weather()
+        {
+            String url = "http://api.openweathermap.org/data/2.5/weather?lat=49.016667&lon=0.65&appid=b7d1a5da596d0f045ad99a42e9d40d57&units=Imperial";
+
+            JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONObject main_object = response.getJSONObject("main");
+                        JSONArray array = response.getJSONArray("weather");
+                        JSONObject object = array.getJSONObject(0);
+                        String temp = String.valueOf(main_object.getDouble("temp"));
+                        String description = object.getString("description");
+                        String city = response.getString("name");
+
+                        //  t1_temp.setText(temp);
+                        t2_city.setText("Smeo");
+                        t3_description.setText(description);
+
+                        if (description.equals("scattered clouds")) {
+                            icone.setImageResource(R.drawable.icon_scatteredclouds);
+                        }
+                        if (description.equals("clear sky")) {
+                            icone.setImageResource(R.drawable.icon_clearsky);
+                        }
+
+                        if (description.equals("broken clouds")) {
+                            icone.setImageResource(R.drawable.icon_brokenclouds);
+                        }
+
+                        if (description.equals("overcast clouds")) {
+                            icone.setImageResource(R.drawable.icon_brokenclouds);
+                        }
+
+                        if (description.equals("rain")) {
+                            icone.setImageResource(R.drawable.icon_rain);
+                        }
+
+                        if (description.equals("snow")) {
+                            icone.setImageResource(R.drawable.icon_snow);
+                        }
+
+                        if (description.equals("mist")) {
+                            icone.setImageResource(R.drawable.icon_mist);
+                        }
+
+                        Calendar calendar = Calendar.getInstance();
+                        SimpleDateFormat sdf = new SimpleDateFormat("EEEE dd/MM", Locale.FRANCE);
+                        String formatted_date = sdf.format(calendar.getTime());
+
+                        t4_date.setText(formatted_date);
+
+                        double temp_int = Double.parseDouble(temp);
+                        double centi = (temp_int - 32) / 1.8000;
+                        centi = Math.round(centi);
+                        int i = (int) centi;
+                        t1_temp.setText(String.valueOf(i));
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }
+            );
+            RequestQueue queue = Volley.newRequestQueue(this);
+            queue.add(jor);
+        }
+
+
+        }
+
+
